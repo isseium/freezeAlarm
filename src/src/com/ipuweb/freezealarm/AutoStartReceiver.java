5c26f9f760fdcbdffd0eb4170c4e6dcbc3c4c7fe
SASAKI Takuya <s2k1ta98@gmail.com>
2012-08-08 22:51:54 +0900

    add condition machine booted

--- src/src/com/ipuweb/freezealarm/AutoStartReceiver.java
+++ src/src/com/ipuweb/freezealarm/AutoStartReceiver.java
@@ -26,11 +26,15 @@ public class AutoStartReceiver extends BroadcastReceiver {
     	
     	// Android 電源ON時に自動でサービスを起動
     	// TODO:isseium actionName を調べてifを設定
-    	Intent serviceIntent = new Intent(context, FreezeAlarmService.class);
-	    context.startService(serviceIntent);
+    	
+    	// 端末起動起動したときはサービスを作成する
+    	if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+    		Intent serviceIntent = new Intent(context, FreezeAlarmService.class);
+    		context.startService(serviceIntent);
+    	}
 	    
 	    // AlarmManager からの受信（freezeAlarm)
-	    if(intent.getAction().equals("freezeAlarm")){
+    	else if(intent.getAction().equals("freezeAlarm")){
 	    	Log.d("freezeAlart", "Alarm");
 			WeatherAccessor wa = new WeatherAccessor();
 			
